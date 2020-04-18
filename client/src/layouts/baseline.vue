3e149ea6b90f3089ff3e77295f84aac6b403d7a0
hassnian <hassnianxd@gmail.com>
2020-04-18 12:33:06 +0200

    first commit

---
+++ client/src/layouts/baseline.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-item link to="/">
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+
+        <v-list-item link to="/videos">
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Videos</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" to />
+      <v-toolbar-title>YT PRE-DASHBOARD</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container>
+        <slot />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String,
+  },
+  data: () => ({
+    drawer: false,
+  }),
+};
+</script>
---
+++ client/src/views/videos/Videos.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1>all videos here</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Videos',
+};
+</script>
+
+<style scoped>
+
+</style>
